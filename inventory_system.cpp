586d9a8e40392bffe02aa0ee2a633601b6752ced
saryu <168134079+saryu4u@users.noreply.github.com>
2024-11-30 14:25:32 +0800

    Add files via upload
    new feature

--- inventory_system.cpp
+++ inventory_system.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <list>
+#include <vector>
 using namespace std;
 
 class inventory {
 private:
     string itemName, description;
-    int quantity, itenId;
+    int quantity, itemId;
     char option;
-    list<string> itemNames;
-    list<string> itemDescriptions;
-    list<int> itemIds;
-    list<int> itemQuantities;
+    // list<string> itemNames;
+    // list<string> itemDescriptions;
+    // list<int> itemIds;
+    // list<int> itemQuantities;
+    vector<int> itemIds;
+    vector<string> itemNames;
+    vector<int> itemQuantities;
+    vector<string> itemDescriptions;
 public:
     void displayMenu() {
         cout << "CTI Inventory System\n\n"
@@ -28,11 +33,61 @@ public:
     }
     void showResultOption(char option) {
         switch (option) {
-            case '1': cout << "Add item" << endl; break;
+            case '1': cout << "Add item\n\n"; addItemToList(); break;
             case '2': cout << "Remove item" << endl; break;
-            case '3': cout << "List items" << endl; break;
+            case '3': cout << "List items\n\n"; showItems(); break;
             case '4': cout << "Quit" << endl; exit(0);
-            default: cout << "You have entered invalid input"; getOption(); break;
+            default: cout << "You have entered invalid input\n"; getOption(); break;
+        }
+    }
+
+    void showItems() {
+
+        if (itemIds.size() != itemNames.size() || itemIds.size() != itemQuantities.size() || itemIds.size() != itemDescriptions.size()) {
+            cout << "Error: Item data is inconsistent." << endl;
+            return;
+        }
+
+        cout << "ID\tName\tQuantity\tDescription" << endl;
+        cout << "---------------------------------------------" << endl;
+
+        for (size_t i = 0; i < itemIds.size(); ++i) {
+            cout << itemIds[i] << "\t"
+                 << itemNames[i] << "\t"
+                 << itemQuantities[i] << "\t\t"
+                 << itemDescriptions[i] << "\n\n";
+        }
+        displayMenu();
+    }
+    bool validateId(int id) {
+        for (size_t i = 0; i < itemIds.size(); ++i) {
+            if(itemIds.at(i) == id) {
+                return false;
+            }
+         }
+        return true;
+    }
+    void addItemToList() {
+        cout << "Enter item name: ";
+        cin >> itemName;
+        cout << "Enter item quantity: ";
+        cin >> quantity;
+        cout << "Enter item id: ";
+        cin >> itemId;
+        cout << "Enter item description: ";
+        cin >> description;
+
+        if(validateId(itemId)) {
+            itemNames.push_back(itemName);
+            itemQuantities.push_back(quantity);
+            itemIds.push_back(itemId);
+            itemDescriptions.push_back(description);
+
+            cout << "Added Successfully\n\n";
+            displayMenu();
+        }else {
+            cout << "THE ID YOU HAVE ENTERED IS ALREADY EXISTED. PLS TRY AGAIN\n\n";
+            addItemToList();
         }
     }
 };

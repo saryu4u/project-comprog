8cd96b06ad7b265ab8cc3d4d370df34c669bf9bf
saryu <168134079+saryu4u@users.noreply.github.com>
2024-11-30 21:17:16 +0800

    Add files via upload
    added a remove function

--- inventory_system.cpp
+++ inventory_system.cpp
@@ -7,15 +7,13 @@ class inventory {
 private:
     string itemName, description;
     int quantity, itemId;
-    char option;
-    // list<string> itemNames;
-    // list<string> itemDescriptions;
-    // list<int> itemIds;
-    // list<int> itemQuantities;
+    size_t index;
+
     vector<int> itemIds;
     vector<string> itemNames;
     vector<int> itemQuantities;
     vector<string> itemDescriptions;
+
 public:
     void displayMenu() {
         cout << "CTI Inventory System\n\n"
@@ -27,18 +25,31 @@ public:
         getOption();
     }
     void getOption() {
-        cout << "Choose your option: ";
-        cin >> option;
+        int option;
+        while (true) {
+            cout << "Choose your option: ";
+            cin >> option;
+
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input, please try again.\n";
+            } else {
+                break;
+            }
+        }
         showResultOption(option);
     }
-    void showResultOption(char option) {
+
+    void showResultOption(int option) {
         switch (option) {
-            case '1': cout << "Add item\n\n"; addItemToList(); break;
-            case '2': cout << "Remove item" << endl; break;
-            case '3': cout << "List items\n\n"; showItems(); break;
-            case '4': cout << "Quit" << endl; exit(0);
-            default: cout << "You have entered invalid input\n"; getOption(); break;
+            case 1: cout << "Add item\n\n"; addItemToList(); break;
+            case 2: cout << "Remove item\n\n"; removeItemFromList(); break;
+            case 3: cout << "List items\n\n"; showItems(); break;
+            case 4: cout << "Quit" << endl; exit(0);
+            default: cout << "Invalid input, please try again.\n"; getOption();
         }
+
     }
 
     void showItems() {
@@ -48,19 +59,19 @@ public:
             return;
         }
 
-        cout << "ID\tName\tQuantity\tDescription" << endl;
+        cout << "ID\t\tName\t\tQuantity\t\tDescription" << endl;
         cout << "---------------------------------------------" << endl;
 
         for (size_t i = 0; i < itemIds.size(); ++i) {
-            cout << itemIds[i] << "\t"
-                 << itemNames[i] << "\t"
-                 << itemQuantities[i] << "\t\t"
+            cout << itemIds[i] << "\t\t"
+                 << itemNames[i] << "\t\t\t"
+                 << itemQuantities[i] << "\t\t\t\t"
                  << itemDescriptions[i] << "\n\n";
         }
         displayMenu();
     }
     bool validateId(int id) {
-        for (size_t i = 0; i < itemIds.size(); ++i) {
+        for (int i = 0; i < itemIds.size(); ++i) {
             if(itemIds.at(i) == id) {
                 return false;
             }
@@ -89,6 +100,25 @@ public:
             cout << "THE ID YOU HAVE ENTERED IS ALREADY EXISTED. PLS TRY AGAIN\n\n";
             addItemToList();
         }
+    }
+    void removeItemFromList() {
+        cout << "Enter item ID to remove: ";
+        int idToRemove;
+        cin >> idToRemove;
+
+        auto it = find(itemIds.begin(), itemIds.end(), idToRemove);
+        if (it != itemIds.end()) {
+            index = distance(itemIds.begin(), it);
+            itemIds.erase(it);
+            itemNames.erase(itemNames.begin() + index);
+            itemQuantities.erase(itemQuantities.begin() + index);
+            itemDescriptions.erase(itemDescriptions.begin() + index);
+
+            cout << "Item with ID " << idToRemove << " removed successfully.\n\n";
+        } else {
+            cout << "Item with ID " << idToRemove << " not found.\n\n";
+        }
+        displayMenu();
     }
 };
 
